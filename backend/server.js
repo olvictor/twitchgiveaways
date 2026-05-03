43815bdf01a269da0e025dccb8288f1be2b2e0a1
victorolv <victorolvdev@gmail.com>
2026-05-03 09:33:37 -0300

    update cors url

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 // Configuração do Socket.io permitindo o frontend conectar
 const io = new Server(server, {
   cors: {
-    origin: 'https://twitchgiveaways-production.up.railway.app/', // Em produção, coloque a URL do seu frontend (ex: http://lhttps://twitchgiveaways-production.up.railway.app/)
+    origin: 'https://twitchgiveaways-production.up.railway.app', // Em produção, coloque a URL do seu frontend (ex: http://lhttps://twitchgiveaways-production.up.railway.app/)
     methods: ['GET', 'POST'],
   },
 });
